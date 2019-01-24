3d6482f8f9909a83e5aaac52782799e260331cc5
Alan Kennard <alan.kennard@almatech.co.uk>
2019-01-24 18:23:19 +0000

    Updated Range Tests

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -21,8 +21,11 @@ namespace tests
         public void SortShouldOrderResults()
         {
             var range = new Range { Count = 3, Sort = true };
+
             var values = new[] { "a", "c", "b" };
+
             var counter = 0;
+
             var generated = range.Of(() => values[counter++]);
 
             Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
