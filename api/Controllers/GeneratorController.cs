d2c9f12624c1daa5b416badcf420b3c2dafddb8e
Alan Kennard <alan.kennard@almatech.co.uk>
2019-02-22 11:54:18 +0000

    Test result update

--- api/Controllers/GeneratorController.cs
+++ api/Controllers/GeneratorController.cs
@@ -24,7 +24,7 @@ namespace api.Controllers
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress("Generator", "generator@generate.com"));
             message.To.Add(new MailboxAddress("", email));
-            message.Subject = "Here are some random names for you";
+            message.Subject = "Here are some random names for you 2";
 
             message.Body = new TextPart("plain")
             {
