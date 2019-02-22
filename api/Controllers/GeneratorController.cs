64067b8ab9c84cdbdaea6c651efd8e1a0aa137e0
Alan Kennard <alan.kennard@almatech.co.uk>
2019-02-22 11:48:24 +0000

    Minor change

--- api/Controllers/GeneratorController.cs
+++ api/Controllers/GeneratorController.cs
@@ -24,7 +24,7 @@ namespace api.Controllers
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress("Generator", "generator@generate.com"));
             message.To.Add(new MailboxAddress("", email));
-            message.Subject = "Here are some random names";
+            message.Subject = "Here are some random names for you";
 
             message.Body = new TextPart("plain")
             {
